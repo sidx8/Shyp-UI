3e4fb1f96b32f97dcafb99295c26a7db046b3de3
siddharth <siddharth0sinha@gmail.com>
2023-05-24 13:02:56 +0530

    add truck status

---
+++ lib/components/customsteper.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+class CustomStepper extends StatelessWidget {
+  final int currentStep;
+  final Color activeColor;
+  final Color inactiveColor;
+
+  CustomStepper({
+    required this.currentStep,
+    this.activeColor = Colors.blue,
+    this.inactiveColor = Colors.grey,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: <Widget>[
+        _buildStep(1),
+        _buildStep(2),
+        _buildStep(3),
+        _buildStep(4),
+        _buildStep(5),
+      ],
+    );
+  }
+
+  Widget _buildStep(int step) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                color: step <= currentStep ? activeColor : inactiveColor,
+                shape: BoxShape.circle,
+              ),
+              width: 12.0,
+              height: 12.0,
+            ),
+            if (step < 5)
+              Container(
+                height: 2.0,
+                width: 50,
+                color: step < currentStep ? activeColor : inactiveColor,
+              ),
+            if (step == 5)
+              Container(
+                height: 2.0,
+                width: 30,
+                color: Colors.transparent,
+              ),
+          ],
+        ),
+        const SizedBox(height: 4.0),
+        Text('Step $step'),
+      ],
+    );
+  }
+}
--- lib/track.dart
+++ lib/track.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import 'components/customsteper.dart';
 import 'confirm.dart';
 
 class TrackLoadScreen extends StatelessWidget {
@@ -32,200 +33,229 @@ class TrackLoadScreen extends StatelessWidget {
         backgroundColor: Colors.white,
       ),
       backgroundColor: Colors.grey[200],
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(15.0),
-            child: Container(
-              padding: const EdgeInsets.all(10),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(10),
-              ),
-              width: double.infinity,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text('Pickup Location',
-                              style: TextStyle(
-                                  color: Colors.grey[700],
-                                  fontWeight: FontWeight.w400)),
-                          Text('${order['pickupLocation']}',
-                              style: TextStyle(
-                                  color: Colors.blueGrey[700],
-                                  fontWeight: FontWeight.w400)),
-                          const SizedBox(height: 20),
-                          Text('Drop Location',
-                              style: TextStyle(
-                                  color: Colors.grey[700],
-                                  fontWeight: FontWeight.w400)),
-                          Text('${order['dropLocation']}',
-                              style: TextStyle(
-                                  color: Colors.blueGrey[700],
-                                  fontWeight: FontWeight.w400)),
-                        ],
-                      ),
-                      Icon(Icons.local_shipping_outlined,
-                          color: Colors.grey[900], size: 100),
-                    ],
-                  ),
-                  const SizedBox(height: 10),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text('Order ID',
-                          style: TextStyle(
-                              color: Colors.grey[700],
-                              fontWeight: FontWeight.w400)),
-                      Text('${order['id']}',
-                          style: TextStyle(
-                              color: Colors.blueGrey[700],
-                              fontWeight: FontWeight.w400)),
-                    ],
-                  ),
-                  const SizedBox(height: 10),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text('PickUp Date',
-                          style: TextStyle(
-                              color: Colors.grey[700],
-                              fontWeight: FontWeight.w400)),
-                      Text('02-feb-23',
-                          style: TextStyle(
-                              color: Colors.blueGrey[700],
-                              fontWeight: FontWeight.w400)),
-                    ],
-                  ),
-                  const SizedBox(height: 10),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text('Preferd Vehicle',
-                          style: TextStyle(
-                              color: Colors.grey[700],
-                              fontWeight: FontWeight.w400)),
-                      Text('Open Half Body truck',
-                          style: TextStyle(
-                              color: Colors.blueGrey[700],
-                              fontWeight: FontWeight.w400)),
-                    ],
-                  ),
-                  const SizedBox(height: 10),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text('Quantity of Load',
-                          style: TextStyle(
-                              color: Colors.grey[700],
-                              fontWeight: FontWeight.w400)),
-                      Text('25 Tons',
-                          style: TextStyle(
-                              color: Colors.blueGrey[700],
-                              fontWeight: FontWeight.w400)),
-                    ],
-                  ),
-                  const SizedBox(height: 10),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text('Material Type',
-                          style: TextStyle(
-                              color: Colors.grey[700],
-                              fontWeight: FontWeight.w400)),
-                      Text('TMT',
-                          style: TextStyle(
-                              color: Colors.blueGrey[700],
-                              fontWeight: FontWeight.w400)),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(15.0),
-            child: Container(
-              padding: const EdgeInsets.all(10),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(10),
-              ),
-              width: double.infinity,
-              child: Column(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Container(
+                padding: const EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                width: double.infinity,
+                child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text(
-                      'Per Tonne Freight Price',
-                      style: TextStyle(
-                          fontWeight: FontWeight.w600,
-                          color: Colors.grey[800],
-                          fontSize: 16),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text('Pickup Location',
+                                style: TextStyle(
+                                    color: Colors.grey[700],
+                                    fontWeight: FontWeight.w400)),
+                            Text('${order['pickupLocation']}',
+                                style: TextStyle(
+                                    color: Colors.blueGrey[700],
+                                    fontWeight: FontWeight.w400)),
+                            const SizedBox(height: 20),
+                            Text('Drop Location',
+                                style: TextStyle(
+                                    color: Colors.grey[700],
+                                    fontWeight: FontWeight.w400)),
+                            Text('${order['dropLocation']}',
+                                style: TextStyle(
+                                    color: Colors.blueGrey[700],
+                                    fontWeight: FontWeight.w400)),
+                          ],
+                        ),
+                        Icon(Icons.local_shipping_outlined,
+                            color: Colors.grey[900], size: 100),
+                      ],
                     ),
-                    Text(
-                      '₹ 3000',
-                      style: TextStyle(
-                          fontWeight: FontWeight.w600,
-                          color: Colors.grey[800],
-                          fontSize: 20),
+                    const SizedBox(height: 10),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text('Order ID',
+                            style: TextStyle(
+                                color: Colors.grey[700],
+                                fontWeight: FontWeight.w400)),
+                        Text('${order['id']}',
+                            style: TextStyle(
+                                color: Colors.blueGrey[700],
+                                fontWeight: FontWeight.w400)),
+                      ],
                     ),
-                  ]),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(15.0),
-            child: SizedBox(
-              width: double.infinity,
-              height: 50,
-              child: ElevatedButton(
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all<Color?>(
-                    Colors.green,
-                  ),
+                    const SizedBox(height: 10),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text('PickUp Date',
+                            style: TextStyle(
+                                color: Colors.grey[700],
+                                fontWeight: FontWeight.w400)),
+                        Text('02-feb-23',
+                            style: TextStyle(
+                                color: Colors.blueGrey[700],
+                                fontWeight: FontWeight.w400)),
+                      ],
+                    ),
+                    const SizedBox(height: 10),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text('Preferd Vehicle',
+                            style: TextStyle(
+                                color: Colors.grey[700],
+                                fontWeight: FontWeight.w400)),
+                        Text('Open Half Body truck',
+                            style: TextStyle(
+                                color: Colors.blueGrey[700],
+                                fontWeight: FontWeight.w400)),
+                      ],
+                    ),
+                    const SizedBox(height: 10),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text('Quantity of Load',
+                            style: TextStyle(
+                                color: Colors.grey[700],
+                                fontWeight: FontWeight.w400)),
+                        Text('25 Tons',
+                            style: TextStyle(
+                                color: Colors.blueGrey[700],
+                                fontWeight: FontWeight.w400)),
+                      ],
+                    ),
+                    const SizedBox(height: 10),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text('Material Type',
+                            style: TextStyle(
+                                color: Colors.grey[700],
+                                fontWeight: FontWeight.w400)),
+                        Text('TMT',
+                            style: TextStyle(
+                                color: Colors.blueGrey[700],
+                                fontWeight: FontWeight.w400)),
+                      ],
+                    ),
+                  ],
                 ),
-                child: const Text('Confirm Booking'),
-                onPressed: () {
-                  showModalBottomSheet(
-                    shape: const RoundedRectangleBorder(
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(20),
-                        topRight: Radius.circular(20),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 15.0, right: 15.0),
+              child: Container(
+                padding: const EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                width: double.infinity,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Load Status',
+                        style: TextStyle(
+                            fontSize: 15,
+                            color: Colors.grey[800],
+                            fontWeight: FontWeight.w600)),
+                    const SizedBox(height: 10),
+                    CustomStepper(
+                      currentStep: 2,
+                      activeColor: Colors.green,
+                      inactiveColor: Colors.red,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Container(
+                padding: const EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                width: double.infinity,
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Per Tonne Freight Price',
+                        style: TextStyle(
+                            fontWeight: FontWeight.w600,
+                            color: Colors.grey[800],
+                            fontSize: 16),
                       ),
+                      Text(
+                        '₹ 3000',
+                        style: TextStyle(
+                            fontWeight: FontWeight.w600,
+                            color: Colors.grey[800],
+                            fontSize: 20),
+                      ),
+                    ]),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: SizedBox(
+                width: double.infinity,
+                height: 50,
+                child: ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color?>(
+                      Colors.green,
                     ),
-                    context: context,
-                    builder: (ctx) => const ConfirmBookingDialog(),
-                  );
-                },
+                  ),
+                  child: const Text('Confirm Booking'),
+                  onPressed: () {
+                    showModalBottomSheet(
+                      shape: const RoundedRectangleBorder(
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(20),
+                          topRight: Radius.circular(20),
+                        ),
+                      ),
+                      context: context,
+                      builder: (ctx) => const ConfirmBookingDialog(),
+                    );
+                  },
+                ),
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(15, 0, 15, 15),
-            child: SizedBox(
-              width: double.infinity,
-              height: 50,
-              child: OutlinedButton(
-                style: OutlinedButton.styleFrom(
-                  backgroundColor: Colors.transparent,
-                  side: const BorderSide(color: Colors.red, width: 2),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(15, 0, 15, 15),
+              child: SizedBox(
+                width: double.infinity,
+                height: 50,
+                child: OutlinedButton(
+                  style: OutlinedButton.styleFrom(
+                    backgroundColor: Colors.transparent,
+                    side: const BorderSide(color: Colors.red, width: 2),
+                  ),
+                  child: const Text(
+                    'Cancel ',
+                    style: TextStyle(color: Colors.red),
+                  ),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
                 ),
-                child: const Text(
-                  'Cancel ',
-                  style: TextStyle(color: Colors.red),
-                ),
-                onPressed: () {
-                  Navigator.pop(context);
-                },
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
